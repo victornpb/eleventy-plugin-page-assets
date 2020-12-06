30971df941bfaa6e90ff44623e76d73918c5e30f
victornpb <3372598+victornpb@users.noreply.github.com>
2020-12-06 15:36:42 -0300

    implemented hashing assets

---
+++ src/hashFile.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+const fs = require('fs');
+
+module.exports = function fileHash(filename, algorithm = "md5") {
+  return new Promise((resolve, reject) => {
+    // Algorithm depends on availability of OpenSSL on platform
+    // Another algorithms: 'sha1', 'md5', 'sha256', 'sha512' ...
+    let shasum = crypto.createHash(algorithm);
+    try {
+      let s = fs.ReadStream(filename);
+      s.on("data", function (data) {
+        shasum.update(data);
+      });
+      // making digest
+      s.on("end", function () {
+        const hash = shasum.digest("hex");
+        return resolve(hash);
+      });
+    } catch (error) {
+      return reject(error);
+    }
+  });
+}
